1f62d491a9ebb0740ba6d7356f3b669a53f61a6d
dtrung98 <trung17dinhle@gmail.com>
2020-07-06 23:59:45 +0700

    add master screen

--- app/src/main/java/com/alikit/news/ui/MainActivity.kt
+++ app/src/main/java/com/alikit/news/ui/MainActivity.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import com.alikit.news.ui.base.BaseActivity
+import com.alikit.news.ui.home.HomeScreen
 import com.ldt.navigation.NavigationFragment
 import com.ldt.navigation.router.FlexRouter
 import com.ldt.navigation.router.SplitRouter
 import com.ldt.navigation.router.SplitRouterSaver
 
 class MainActivity : BaseActivity() {
-    override fun provideDefaultDetailFragment(): Class<out NavigationFragment> {
-        TODO("Not yet implemented")
-    }
-
-    override fun provideDefaultMasterFragment(): Class<out NavigationFragment> {
-        TODO("Not yet implemented")
-    }
-
+    override fun provideDefaultDetailFragment(): Class<out NavigationFragment> = HomeScreen::class.java
+    override fun provideDefaultMasterFragment(): Class<out NavigationFragment> = HomeScreen::class.java
 }
